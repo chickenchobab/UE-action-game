e25b9b40a5fa575cb3311ad86c0e858c4b59edbc
chickenchobab <hew980812@gmail.com>
2025-05-07 20:15:13 +0900

    Use stamina to dodge

--- Source/Slash/Private/Components/AttributeComponent.cpp
+++ Source/Slash/Private/Components/AttributeComponent.cpp
@@ -23,3 +23,14 @@ void UAttributeComponent::ReceiveDamage(float Damage)
 {
 	Health = FMath::Clamp(Health - Damage, 0.f, MaxHealth);
 }
+
+void UAttributeComponent::UseStamina(float StaminaCost)
+{
+	Stamina = FMath::Clamp(Stamina - StaminaCost, 0.f, MaxStamina);
+}
+
+
+void UAttributeComponent::RegenStamina(float DeltaTime)
+{
+	Stamina = FMath::Clamp(Stamina + StaminaRegenRate * DeltaTime, 0.f, MaxStamina);
+}
--- Source/Slash/Public/Components/AttributeComponent.h
+++ Source/Slash/Public/Components/AttributeComponent.h
@@ -18,16 +18,21 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	void ReceiveDamage(float Damage);
+	void UseStamina(float StaminaCost);
+	void RegenStamina(float DeltaTime);
 
 	FORCEINLINE float GetHealthPercent() const { return Health / MaxHealth; }
+	FORCEINLINE float GetStaminaPercent() const { return Stamina / MaxStamina; }
 
 	UFUNCTION(BlueprintCallable)
 	bool IsAlive() const { return Health > 0.f; }
 
 	FORCEINLINE void AddGold(int32 AmountOfGold) { Gold += AmountOfGold; }
-	void AddSouls(int32 NumberOfSouls) { Souls += NumberOfSouls; }
+	FORCEINLINE void AddSouls(int32 NumberOfSouls) { Souls += NumberOfSouls; }
 	FORCEINLINE int32 GetGold() const { return Gold; }
 	FORCEINLINE int32 GetSouls() const { return Souls; }
+	FORCEINLINE float GetDodgeCost() const { return DodgeCost; }
+	FORCEINLINE float GetStamina() const { return Stamina; }
 
 protected:
 	virtual void BeginPlay() override;
@@ -35,10 +40,20 @@ protected:
 private:
 	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float Health;
-
 	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	float MaxHealth;
 
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float Stamina;
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float MaxStamina;
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float StaminaRegenRate = 5.f;
+
+	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
+	float DodgeCost = 10.f;
+
+	
 	UPROPERTY(EditAnywhere, Category = "Actor Attributes")
 	int32 Gold;
 
